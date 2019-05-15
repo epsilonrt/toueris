d22a78760f1e4344ebe061223e3721bae8c78db2
epsilonrt <epsilonrt@gmail.com>
2019-05-15 13:53:42 +0200

    Replace WireHmi by Hmi4DinBox

--- src/arduino/toueris-ctor/toueris-ctor.ino
+++ src/arduino/toueris-ctor/toueris-ctor.ino
@@ -4,8 +4,10 @@
 #include <EEPROM.h>
 #include <Modbus.h>
 #include <ModbusSerial.h> // https://github.com/epsilonrt/modbus-arduino
-#include <Toueris2Hmi.h>  // https://github.com/epsilonrt/WireHmi
-#include <LCD_ST7032.h> // https://github.com/epsilonrt/LCD_ST7032
+#include <Hmi4DinBox.h>  // https://github.com/epsilonrt/WireHmi
+
+//#include <Toueris2Hmi.h>  // https://github.com/epsilonrt/WireHmi
+//#include <LCD_ST7032.h> // https://github.com/epsilonrt/LCD_ST7032
 #include <PinChangeInterrupt.h> // https://github.com/NicoHood/PinChangeInterrupt
 #include "bisrelay.h"
 #include "version.h" // for update this file: $ git-version version.h 
@@ -15,6 +17,7 @@ const byte MODBUS_SLAVEID = 8;
 const unsigned long MODBUS_BAUDRATE = 19200;
 const byte MODBUS_CONFIG = MB_PARITY_EVEN;
 const char MODBUS_STR_CONFIG[] = "8E1";
+const char MODBUS_SLAVEID_DATA[] = "CTOR";
 
 // Backlight
 const byte eeBl = 0; // address in EEPROM
@@ -40,9 +43,9 @@ BisRelay rl[5] = {
   BisRelay (29, 25)
 };
 // I2c Human-Machine Interface
-Toueris2Hmi hmi (hirqPin);
+Hmi4DinBox hmi (hirqPin);
 // ST7032 LCD
-LCD_ST7032 lcd;
+//LCD_ST7032 lcd;
 
 // Global variables
 volatile bool icomChange = false;
@@ -64,31 +67,24 @@ void icomHandler() {
 }
 
 void setup() {
-  int loops = 0;
 
-  // starts the LCD, need to repeat the call in the case of a USB power boot...
-  for (int i = 0; i < 2; i++) {
-    lcd.begin();  // this function calls Wire.begin(), no need to double the call!
+  while (!hmi.begin(24)) { // start the HMI by checking that it has worked well...
+    delay (500);
   }
-  lcd.setcontrast (24); // contrast value range is 0-63, try 25@5V or 50@3.3V as a starting value
+
   //          0123456789012345
-  lcd.print ("CTOR v");
-  lcd.print (VERSION_SHORT);
-  lcd.setCursor (1, 0);
-  lcd.print ("Waiting HMI...");
+  hmi.lcd.print ("CTOR v");
+  hmi.lcd.print (VERSION_SHORT);
+  hmi.lcd.setCursor (1, 0);
+  hmi.lcd.print ("Waiting HMI...");
 
   dev = EEPROM.read (eeDev);
   if (dev & 0xE0) {
     dev = 0;
     EEPROM.write (eeDev, dev);
   }
-  // lcd.print (dev, HEX);
+  // hmi.lcd.print (dev, HEX);
 
-  while (!hmi.begin()) { // start the HMI by checking that it has worked well...
-    lcd.setCursor (1, 0);
-    lcd.print (++loops);
-    delay (500);
-  }
 
   bl = EEPROM.read (eeBl) & 0xF8; // rounded to a multiple of 8
   hmi.backlight.write (bl);
@@ -98,14 +94,15 @@ void setup() {
   mb.config (&Serial1, MODBUS_BAUDRATE, MODBUS_CONFIG, dePin);
   // Set the Slave ID (1-247)
   mb.setSlaveId (MODBUS_SLAVEID);
+  mb.setAdditionalServerData (MODBUS_SLAVEID_DATA); // for Report Server ID function
 
-  lcd.clear();
+
+  hmi.lcd.clear();
   //          0123456789012345
-  lcd.print ("12345| CTOR @");
-  lcd.print (MODBUS_SLAVEID);
+  hmi.lcd.print ("12345| CTOR @");
+  hmi.lcd.print (MODBUS_SLAVEID);
 
-
-  lcd.setCursor (1, 0);
+  hmi.lcd.setCursor (1, 0);
   for (int i = 0; i < 5; i++) {
 
     mb.addCoil (i);
@@ -115,18 +112,18 @@ void setup() {
       mb.Coil (i, true);
     }
 
-    lcd.write (rl[i].state() ? 'X' : '-');
+    hmi.lcd.write (rl[i].state() ? 'X' : '-');
   }
 
   //          0123456789
-  lcd.print ("| ");
-  lcd.print (MODBUS_BAUDRATE);
-  lcd.print (MODBUS_STR_CONFIG);
+  hmi.lcd.print ("| ");
+  hmi.lcd.print (MODBUS_BAUDRATE);
+  hmi.lcd.print (MODBUS_STR_CONFIG);
 
   attachPCINT (digitalPinToPCINT (icomPin), icomHandler, CHANGE);
   hmi.led.set (LED_GREEN1);
-  lcd.cursor();
-  lcd.blink();
+  hmi.lcd.cursor();
+  hmi.lcd.blink();
 }
 
 void loop() {
@@ -180,7 +177,7 @@ void loop() {
       manRelay %= 5;
     }
   }
-  lcd.setCursor (1, manRelay);
+  hmi.lcd.setCursor (1, manRelay);
 
   if (blChange) {
 
@@ -213,8 +210,8 @@ void loop() {
         rl[i].reset();
         dev &= ~ (1 << i);
       }
-      lcd.setCursor (1, i);
-      lcd.write (rl[i].state() ? 'X' : '-');
+      hmi.lcd.setCursor (1, i);
+      hmi.lcd.write (rl[i].state() ? 'X' : '-');
       devChange = true;
     }
   }
